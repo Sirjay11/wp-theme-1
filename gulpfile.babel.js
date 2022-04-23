c3028e0cbef6395c2126af1f68cf91ee3652bcfd
James-Leroy <jlanvja@gmail.com>
2022-04-23 08:29:28 -0400

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,6 +16,8 @@ import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 import imagemin from 'gulp-imagemin';
+import del from 'del';
+import webpack from 'webpack-stream';
 
 const imagemins = require("gulp-imagemin");
 
@@ -33,11 +35,17 @@ const paths = {
     src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
     dest: 'dist/assets/images'
   },
+  scrips: {
+    src: 'src/assets/js/bundle.js',
+    dest: 'dist/assets/js'
+  },
   other: {
     src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
-    dest: 'src/assets'
+    dest: 'dist/assets'
   }
 }
+
+export const clean = () => del(['dist']);
 
 export const styles = () => {
   return gulp
@@ -57,11 +65,37 @@ export const images = () => {
 
 export const watch = () => {
   gulp.watch('src/assets/scss/**/*.scss', styles);
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.other.src, copy);
 } 
 
 export const copy = () => {
   return gulp.src(paths.other.src)
-    .pipe(paths.other.dest);
+    .pipe(gulp.dest(paths.other.dest));
 }
 
-//export default hello;
+export const scripts = () => {
+  return gulp.src(paths.scrips.src)
+    .pipe(webpack({
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest(paths.scrips.dest));
+}
+
+export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+
+export default dev;
