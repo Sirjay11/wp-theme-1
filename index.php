211b431ffb7d44f26965836473a14be8d1207d5d
James-Leroy <jlanvja@gmail.com>
2022-04-27 06:59:22 -0400

    update

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php get_header(); ?>
-
+<div class="o-container u-margin-bottom-40">
+  <div class="o-row">
+    <div class="o-row__column o-row__column--span-12 o-row__column--span-8@medium">
+      <main role="main">
 <?php if(have_posts()) { ?>
     <?php while(have_posts()) { ?>
     <?php the_post(); ?>
     <h2>
-     <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
+      <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
     </h2>
 
     <div>
@@ -22,7 +25,12 @@
 <?php } else { ?>
     <p><?php echo apply_filters('_themename_no_posts_text', esc_html__('Sorry, no posts matched criteria.','_themename')); ?></p>
 <?php } ?>
-
-
+</main>
+</div>
+<div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
+  <?php> get_sidebar(); ?>
+</div>
+</div>
+</div>
 
 <?php get_footer(); ?>
